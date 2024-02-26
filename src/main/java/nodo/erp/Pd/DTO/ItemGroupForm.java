f02aed93ec17451077181efb2e8d3e7a0f90203d
eunjeong901 <151818688+eunjeong901@users.noreply.github.com>
2024-02-26 17:41:39 +0900

    20240226 / 사이드바 메뉴트리·아이콘 정리, 수정·삭제 svg 추가

--- src/main/java/nodo/erp/Pd/DTO/ItemGroupForm.java
+++ src/main/java/nodo/erp/Pd/DTO/ItemGroupForm.java
@@ -1,5 +1,6 @@
-package nodo.erp.Pd;
+package nodo.erp.Pd.DTO;
 
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,6 +9,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ItemGroupForm {
+	
+	@Column(length = 6)
+	private String IgCode;
+	
 	@NotBlank(message="품목그룹명은 필수항목입니다.")
 	@Size(max=20)
 	private String IgName;
--- src/main/java/nodo/erp/Pd/Repository/ItemGroupRepository.java
+++ src/main/java/nodo/erp/Pd/Repository/ItemGroupRepository.java
@@ -1,6 +1,8 @@
-package nodo.erp.Pd;
+package nodo.erp.Pd.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import nodo.erp.Pd.Entity.ItemGroup;
 
 public interface ItemGroupRepository extends JpaRepository<ItemGroup, Integer> {
 
